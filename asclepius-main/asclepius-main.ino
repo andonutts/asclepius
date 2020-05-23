7df63ce6018fccce6ae9d60150bb199901643aa3
Andy He <smerchy@gmail.com>
2020-05-23 17:31:30 -0500

    Create main asclepius control program

---
+++ asclepius-main/asclepius-main.ino
@@ -0,0 +1,79 @@
+#include <RTClib.h>
+#include <DmxSimple.h>
+
+// DIM_DELAY_MS defines the number of milliseconds between each brightness value when fading up/down
+#define DIM_DELAY_MS 1000
+
+// ALARM1_TIME and ALARM2_TIME respectively define the morning and evening alarm times
+#define ALARM1_TIME 600
+#define ALARM2_TIME 1735
+
+RTC_PCF8523 rtc;
+DateTime now;
+int alarm1Triggered;
+int alarm2Triggered;
+
+void fadeUp(int period) {
+    int brightness;
+    for (brightness = 0; brightness <= 255; brightness++) {
+        DmxSimple.write(1, brightness);
+        delay(period);
+    }
+}
+
+void fadeDown(int period) {
+    int brightness;
+    for (brightness = 255; brightness >= 0; brightness--) {
+        DmxSimple.write(1, brightness);
+        delay(period);
+    }
+}
+
+void setup () {
+    // set the DMX module to Master mode
+    pinMode(2, OUTPUT);
+    digitalWrite(2, HIGH);
+
+    // set the pin that the module will receive commands on
+    DmxSimple.usePin(4);
+
+    // set number of channels
+    DmxSimple.maxChannel(1);
+
+    if (!rtc.begin()) {
+        abort();
+    }
+
+    if (!rtc.initialized() || rtc.lostPower()) {
+        // When time needs to be set on a new device, or after a power loss, the
+        // following line sets the RTC to the date & time this sketch was compiled
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
+
+    now = rtc.now();
+    if (now > ALARM1_TIME && now < ALARM2_TIME) {
+        DmxSimple.write(1, 0);
+    } else {
+        DmxSimple.write(1, 255);
+    }
+}
+
+void loop () {
+    // obtain the current time and convert it to a military time integer
+    now = rtc.now();
+    int hourMin = (now.hour() * 100) + now.minute();
+
+    if (hourMin == 0) {
+        // reset all alarmTriggered flags at midnight
+        alarm1Triggered = 0;
+        alarm2Triggered = 0;
+    } else if (hourMin == ALARM1_TIME && !alarm1Triggered) {
+        alarm1Triggered = 1;
+        fadeDown(DIM_DELAY_MS);
+    } else if (hourMin == ALARM2_TIME && !alarm2Triggered) {
+        alarm2Triggered = 1;
+        fadeUp(DIM_DELAY_MS);
+    }
+
+    delay(10000);
+}
